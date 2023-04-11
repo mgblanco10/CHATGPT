18e16ce40a7392f8805441e2e5de3412096f93d4
mgblanco10 <mgblanco1001@gmail.com>
2023-04-11 13:06:46 +0200

    progreso de los metodos basicos de python

--- littleTestsPyton/partsString.py
+++ littleTestsPyton/partsString.py
@@ -33,3 +33,31 @@ def replace_domain(email, old_domain, new_domain):
         new_email = email[:index]+ "@" + new_domain
         return new_email
     return email
+
+# upper and lower
+print("Monica".upper())
+print("Monica".lower())
+
+# espacios
+print("   yes      ".strip())
+print("   yes      ".lstrip())
+print("   yes      ".rstrip())
+
+# contar algún elemento
+print("The number of times e occurs in this string is 4".count("e"))
+
+# devuelve si la cadena termina con una determinada subcadena
+print("Forest".endswith("rest"))
+
+# devuelve si la cadena está formada por sólo números
+print("Forest".isnumeric())
+print("123456".isnumeric())
+# # # # SABIENDO ESTO PODEMOS USAR FUNCIÓN int pra convertirla en un número real
+print(int("12345") + int("54321"))
+
+# concatenar
+print(" ".join(["This", "is", "a", "phrase", "joined", "by", "spaces"]))
+print("...".join(["This", "is", "a", "phrase", "joined", "by", "spaces"]))
+
+# devuelve una lista de las palabras de la cadena inicial
+print("This is another example".split())
