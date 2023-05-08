06b6c1806fa06bfe4a355d24a32cb504d0a6d1f9
mgblanco10 <mgblanco1001@gmail.com>
2023-05-08 17:44:24 +0200

    docstrings in python

--- OOP/constructorsMethods.py
+++ OOP/constructorsMethods.py
@@ -25,3 +25,8 @@ class Apple:
     
 goldengold = Apple("green", "sour")
 print(goldengold)
+# help(Apple)
+
+
+
+
---
+++ OOP/docstrings.py
@@ -0,0 +1,15 @@
+#Docstring
+def to_seconds(hours, minutes, seconds):
+    """Returns the amount of seconds in the given hours, minutes, and seconds."""
+    return hours*3600+minutes*60+seconds
+
+# help(to_seconds)
+
+class Person:
+  def __init__(self, name):
+    self.name = name
+  def greeting(self):
+    """Outputs a message with the name of the person"""
+    print("Hello! My name is {name}.".format(name=self.name)) 
+
+# help(Person.greeting)
